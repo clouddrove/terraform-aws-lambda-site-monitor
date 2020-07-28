8a87e95e43d1e956d7845036ec849dfa6125fdbb
Anmol Nagpal <anmol@clouddrove.com>
2020-07-28 14:56:45 +0530

    update script

--- monitor/src/index.py
+++ monitor/src/index.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import boto3
-import urllib.request as urllib2
+# import urllib.request as urllib2
 import os
 import json
 import threading
+import requests
+from requests.exceptions import HTTPError
 
 website_url           = json.loads(os.environ['Website_URL'])
 metricname            = os.environ['metricname']
@@ -14,73 +16,59 @@ def write_metric(value, metric, site):
 	print(value)
 	d = boto3.client('cloudwatch')
 	d.put_metric_data(Namespace='Website Status',
-	                  MetricData=[
-		                  {
-	                  'MetricName':metric,
-	                  'Dimensions':[
-		                  {
-	                  'Name': 'Status',
-	                  'Value': 'WebsiteStatusCode',
-		                  },
-		                  {
-	                  'Name': 'Website',
-	                  'Value': site,
-		                  },
-		                  ],
-	                  'Value': value,
+					  MetricData=[
+						  {
+					  'MetricName':metric,
+					  'Dimensions':[
+						  {
+					  'Name': 'Status',
+					  'Value': 'WebsiteStatusCode',
+						  },
+						  {
+					  'Name': 'Website',
+					  'Value': site,
+						  },
+						  ],
+					  'Value': value,
 	},
 	]
-	                  )
+					  )
 
 def check_site(url, metric):
 
 	STAT = 1
 	print("Checking %s " % url)
-	request = urllib2.Request(url)
+	# request = urllib2.Request(url)
 	try:
-		response = urllib2.urlopen(request,timeout=timeout)
-		response.close()
-	except urllib2.URLError as e:
-		if hasattr(e, 'code'):
-			print("1st if")
-			print ("[Error:] Connection to %s failed with code: " %url +str(e.code))
-			STAT = int(e.code)
-			# write_metric(STAT, metric, url)
-		elif hasattr(e, 'reason'):
-			print("2nd if")
-			print ("[Error:] Connection to %s failed with code: " % url +str(e.reason))
-			STAT = 501
-			# write_metric(STAT, metric, url)
-	except urllib2.HTTPError as e:
-		if hasattr(e, 'code'):
-			print("3rd if")
-			print ("[Error:] Connection to %s failed with code: " % url + str(e.code))
-			STAT = int(e.code)
-			# write_metric(STAT, metric, url)
-		elif hasattr(e, 'reason'):
-			print("4th if")
-			print ("[Error:] Connection to %s failed with code: " % url + str(e.reason))
-			STAT = 501
-			# write_metric(STAT, metric, url)
-	print(STAT)
+		response = requests.get(url,timeout=timeout)
+		# response.raise_for_status()
+	except HTTPError as http_err:
+		print(http_err)
+		STAT = 501
+	except Exception as err:
+		print(err)
+		STAT = 501
+
 	if STAT != 501 and STAT ==1:
-		STAT = response.getcode()
+		print('Success!')
+		print(response.status_code)
+		STAT = response.status_code
 
 	return STAT
 
 def run_thread(site):
-  	r = check_site(site,metricname)
-  	if r == 200 or r == 304 or r == 400:
-  		print("Site %s is up" %site)
-  		write_metric(200, metricname, site)
-  	else:
-  		print("[Error:] Site %s down" %site)
-  		write_metric(int(r), metricname, site)
+	r = check_site(site,metricname)
+	if r == 200 or r == 304 or r == 400:
+		print("Site %s is up" %site)
+		write_metric(200, metricname, site)
+	else:
+		print("[Error:] Site %s down" %site)
+		write_metric(int(r), metricname, site)
 
-def handler(event, context):
+def handler():
 
 	# Change these to your actual websites.  Remember, the more websites you list
-        # the longer the lambda function will run
+		# the longer the lambda function will run
 	websiteurls = website_url
 	t = [0]*len(website_url)
 	for i in range(len(websiteurls)):
@@ -90,3 +78,5 @@ def handler(event, context):
 	for i in range(len(t)):
 		t[i].join()
 	print("Done!")
+
+handler()
